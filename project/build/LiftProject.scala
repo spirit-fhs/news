9cf798214139425506a97e11d248cc7eb5cdc258
Marcus Denison <denison@stud.fh-sm.de>
2010-12-11 18:35:09 +0100

    Added Project File and Fixed README

---
+++ project/build/LiftProject.scala
@@ -0,0 +1,24 @@
+import sbt._
+
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  val mavenLocal = "Local Maven Repository" at
+  "file://"+Path.userHome+"/.m2/repository"
+
+  val scalatools_release = "Scala Tools Releases" at
+  "http://scala-tools.org/repo-releases"
+  val liftVersion = "2.1"
+
+  override def libraryDependencies = Set(
+    "net.databinder" %% "dispatch" % "0.7.7", 
+    "net.liftweb" %% "lift-textile" % liftVersion % "compile->default" withSources,
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default" withSources,
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default" withSources,
+    "net.liftweb" %% "lift-mongodb" % liftVersion % "compile->default" withSources,
+    "net.liftweb" %% "lift-mongodb-record" % liftVersion % "compile->default" withSources,
+    "junit" % "junit" % "4.5" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
+    "org.mockito" % "mockito-all" % "1.8.0" % "test",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
+
+  ) ++ super.libraryDependencies
+}
