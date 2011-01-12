10e427e02b349e7a516d757d2acd1a687da4f626
Oliver Braun <ob@obraun.net>
2011-01-12 18:21:25 +0100

    * add "[Update]" to email subject * remove trailing space while I'm here

--- src/main/scala/org/unsane/spirit/news/snippet/EditNews.scala
+++ src/main/scala/org/unsane/spirit/news/snippet/EditNews.scala
@@ -54,12 +54,12 @@ class EditNews extends Loggable with SpiritHelpers with Config with EntryPreview
 
   private val tweet = loadProps("Tweet") == "yes"
 
-  /** 
+  /**
    * Views the list of entries from the current user for editing or deleting them.
    */
   def view (xhtml : NodeSeq) : NodeSeq = {
     val news = Entry.findAll("name" -> User.currentUserId.open_!.toString)
-    
+
     news.flatMap(v =>
       <tr><td>{v.nr.value.toString}</td><td>{v.writer.value.toString}</td>
       <td>{v.subject.value.toString}</td><td>{v.date.value.toString.substring(4, 11) + ". " + v.date.value.toString.substring(17, 22)}</td>
@@ -69,7 +69,7 @@ class EditNews extends Loggable with SpiritHelpers with Config with EntryPreview
 
   /**
    * Takes the given parameters, searches for the entry with the given number and saves the new entry with new number.
-   * Would like to use old number, but we would have problem tweeting this with Twitter! 
+   * Would like to use old number, but we would have problem tweeting this with Twitter!
    * @param post the changed post
    * @param nr the entry number
    * @param subject the post subject
@@ -114,12 +114,12 @@ class EditNews extends Loggable with SpiritHelpers with Config with EntryPreview
     logger info "Entry was created by " + User.currentUserId.openOr("")
     logger info "Entry was updated by " + User.currentUserId.openOr("")
     if (sendEmail) MailHandler.send(TextileParser.toHtml(post).toString,
-      subject, loadEmails(semester split (" ")))
+      "[Update] " + subject, loadEmails(semester split (" ")))
     if (tweet && tweetUpdate) Spreader ! Tweet("[Update] " + subject, semester.split(" ").map(" #"+_).mkString , newNr)
     S notice "Ihr update wurde gespeichert"
     S redirectTo "/index"
   }
-  
+
   /**
    * Searches for the entry with the given number an deletes the post
    * @param Entry
@@ -170,7 +170,7 @@ class EditNews extends Loggable with SpiritHelpers with Config with EntryPreview
   }
 
   /**
-   * Creates the view for validating the deletion of an entry 
+   * Creates the view for validating the deletion of an entry
    */
   def confirmDelete(xhtml: Node): NodeSeq = {
     try {
