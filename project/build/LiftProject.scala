1cc34ae82f88b8adb759be1e85b837e64a474ba7
Marcus Denison <denison@stud.fh-sm.de>
2011-09-12 21:00:00 +0200

    Bumped to Scala 2.9.1 git add .git add .

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -9,7 +9,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   val liftVersion = "2.4-M4"
 
   override def libraryDependencies = Set(
-    "net.databinder" %% "dispatch" % "0.7.7", 
+    "net.databinder" %% "dispatch" % "0.7.8", 
     "net.liftweb" %% "lift-textile" % liftVersion % "compile->default" withSources,
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default" withSources,
     "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default" withSources,
@@ -18,7 +18,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "net.liftweb" %% "lift-record" % liftVersion % "compile->default" withSources,
     "net.liftweb" %% "lift-json" % liftVersion % "compile->default" withSources,
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" %% "specs" % "1.6.7" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test->default",
     "org.mockito" % "mockito-all" % "1.8.0" % "test",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "ch.qos.logback" % "logback-classic" % "0.9.26"
