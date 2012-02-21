c23a9ad61bd9a255114b7fc06a668cfd1078c595
Stefan Kunze <stefan@stefan-laptop.(none)>
2012-02-21 16:56:47 +0100

    add issue #28 Extend Rest-Service

---
+++ src/main/scala/org/unsane/spirit/news/model/EntryPreview.scala
@@ -0,0 +1,13 @@
+package org.unsane.spirit.news.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stefan
+ * Date: 19.02.12
+ * Time: 15:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+class EntryPreview {
+
+}
